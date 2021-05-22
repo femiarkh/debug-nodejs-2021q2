8cd5709762573d3276c53824641eb798192d4239
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-05-22 06:38:14 +0300

    Start to solve logic errors (VIP)

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
-
+var game = require('./controllers/gamecontroller');
 
 db.sync();
-app.use(require('body-parser'));
+app.use(express.json());
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(require('./middleware/validate-session'));
 app.use('/api/game', game);
-app.listen(function() {
-    console.log("App is listening on 4000");
-})
+app.listen(process.env.PORT, function () {
+  console.log(`App is listening on ${process.env.PORT}`);
+});
